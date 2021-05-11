0b3d0ae03980717a7b0d02a01e15c016b7f32b0f
pjj885 <xorhkd513@naver.com>
2021-05-11 21:59:36 +0900

    객체지향1

---
+++ src/ch02/Student.java
@@ -0,0 +1,10 @@
+package ch02;
+
+public class Student {
+    int studentNumber;
+    String studenName;
+    int majorCode;
+    String majorName;
+    int grade;
+
+}
---
+++ src/ch03/FunctionTest.java
@@ -0,0 +1,34 @@
+package ch03;
+
+public class FunctionTest {
+
+    public static int addNum(int num1, int num2) {
+        int result;
+        result = num1 + num2;
+        return result;
+    }
+
+    public static void sayHello(String greeting) {
+        System.out.println(greeting);
+    }
+
+    public static int calcSum() {
+        int sum = 0;
+        int i;
+
+        for (i = 0; i <= 100; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        sayHello("박재준 안녕");
+        int n1 = 10;
+        int n2 = 20;
+        int total = addNum(n1, n2);
+        System.out.println(total);
+
+        total = calcSum();
+        System.out.println(total);
+    }
+}
